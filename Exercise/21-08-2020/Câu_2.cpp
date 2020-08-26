6ab8ccc1b9b6a77b8b5ac717ce8520c2e36da7c2
ocsonao <57809918+ocsonao@users.noreply.github.com>
2020-08-27 00:47:20 +0700

    Update Câu_2.cpp

--- Exercise/21-08-2020/Câu_2.cpp
+++ Exercise/21-08-2020/Câu_2.cpp
@@ -1,5 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int 
+int main()
+{
+    float a,b;
+    cout<<"nhap vao a va b : "<<endl;
+    cin>>a>>b;
+    cout<<"tong a+b : "<<a+b<<endl;
+    cout<<"hieu a-b : "<<a-b<<endl;
+    cout<<"hieu b-a : "<<b-a<<endl;
+    cout<<"tich a*b : "<<a*b<<endl;
+    cout<<"thuong a/b : "<<a/b<<endl;
+    cout<<"thuong b/a : "<<b/a<<endl;
 }
