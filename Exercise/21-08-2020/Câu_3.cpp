528d7d9dcadaa36866d374b52b5f5835db59e2f2
ocsonao <57809918+ocsonao@users.noreply.github.com>
2020-08-27 00:48:32 +0700

    Update Câu_3.cpp

--- Exercise/21-08-2020/Câu_3.cpp
+++ Exercise/21-08-2020/Câu_3.cpp
@@ -1,5 +1,11 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
-int main(){
-	int 
+int main()
+{
+    float a,b,c,tbc;
+    cout<<"nhap a,b,c :"<<endl;
+    cin>>a>>b>>c;
+    tbc=(a+b+c)/3;
+    cout<<"trung binh cong cua 3 so vua nhap la : "<<setprecision(5)<<tbc<<endl;
 }
